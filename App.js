033bb29351582e5c4c1a53f81329740aaf1606b8
Akiko Yokoyama <acotie.acotie@gmail.com>
2017-11-12 20:26:30 +0900

    fix rewrite code. React Native ver 0.19 -> 0.49

--- App.js
+++ App.js
@@ -1,29 +1,13 @@
 import React from 'react';
-import { StyleSheet, Text, View, Image } from 'react-native';
+import { StyleSheet, Text, ListView, View, Image } from 'react-native';
 
+var REQUEST_URL = 'https://raw.githubusercontent.com/facebook/react-native/master/docs/MoviesExample.json';
 var MOCKED_MOVIES_DATA = [
     {title: 'Title', year: '2015', posters: {thumbnail: 'http://i.imgur.com/UePbdph.jpg'}},
 ];
+var movie = MOCKED_MOVIES_DATA[0];
 
-export default class App extends React.Component {
-  render() {
-    var movie = MOCKED_MOVIES_DATA[0];
-    return (
-      <View style={styles.container}>
-          <Image 
-            source={{uri: movie.posters.thumbnail}} 
-            style={styles.thumbnail}
-          />
-          <View style={styles.rightContainer}>
-            <Text style={styles.title}>{movie.title}</Text>
-            <Text style={styles.year}>{movie.year}</Text>
-          </View>
-      </View>
-    );
-  }
-}
-
-const styles = StyleSheet.create({
+var styles = StyleSheet.create({
   container: {
     flexDirection: 'row',
     backgroundColor: '#fff',
@@ -44,6 +28,81 @@ const styles = StyleSheet.create({
   },
   year: {
     textAlign: 'center',
-  },
-
+  }
 });
+
+
+export default class App extends React.Component {
+
+  getInitialState() {
+    return {
+      movies: null,
+    };
+  }
+
+  componentDidMount() {
+    this.fetchData();
+  }
+
+  fetchData() {
+    fetch(REQUEST_URL)
+      .then((response) => response.json())
+      .then((responseData) => {
+        this.setState({
+          movies: responseData.movies,
+        });
+      })
+      .done();
+  }
+
+  render() {
+    if (!this.state.movies) {
+      return this.renderLoadingView();
+    }
+
+    var movie = this.state.movies[0];
+    return this.renderMovie(movie);
+  }
+
+  renderLoadingView() {
+    return (
+      <View style={styles.container}>
+        <Text>
+          Loading movies...
+        </Text>
+      </View>
+    );
+  }
+
+  renderMovie(movie) {
+    return (
+      <View style={styles.container}>
+        <Image
+          source={{uri: movie.posters.thumbnail}}
+          style={styles.thumbnail}
+        />
+        <View style={styles.rightContainer}>
+          <Text style={styles.title}>{movie.title}</Text>
+          <Text style={styles.year}>{movie.year}</Text>
+        </View>
+      </View>
+    );
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+          <Image 
+            source={{uri: movie.posters.thumbnail}} 
+            style={styles.thumbnail}
+          />
+          <View style={styles.rightContainer}>
+            <Text style={styles.title}>{movie.title}</Text>
+            <Text style={styles.year}>{movie.year}</Text>
+          </View>
+      </View>
+    );
+  }
+}
+
+
